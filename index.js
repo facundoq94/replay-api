6f6df7af1c0b2658c71733abca1dfaf7239eae93
Facundo Quintana <facunquintana@gmail.com>
2022-05-20 09:42:14 -0300

    test

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
 
+const example = {"menu": {
+    "id": "file",
+    "value": "File",
+    "popup": {
+      "menuitem": [
+        {"value": "New", "onclick": "CreateNewDoc()"},
+        {"value": "Open", "onclick": "OpenDoc()"},
+        {"value": "Close", "onclick": "CloseDoc()"}
+      ]
+    }
+  }};
+
 const port = process.env.PORT || 4000;
 
 app.listen(port);
 
 app.get("/", (req, res) => {
     res.send("home");
+});
+
+app.get("/getPartidos", (req, res) => {
+    res.send(example);
 });
 
 console.log('listen on port', port);
